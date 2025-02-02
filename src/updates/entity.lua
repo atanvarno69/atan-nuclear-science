582f7f646a8945cd4b870fe2790280340794ba60
Simon Brodtmann <github@accounts.brodtmann.info>
2025-02-02 22:54:13 +0100

    Remove bonus productivity for centrifuge

--- src/updates/entity.lua
+++ src/updates/entity.lua
@@ -11,11 +11,6 @@ local centrifuge = data.raw["assembling-machine"]["centrifuge"]
 table.insert(centrifuge.crafting_categories, "centrifuging-or-crafting")
 centrifuge.crafting_speed = 2
 centrifuge.module_slots = 4
-if settings.startup["atan-nuclear-centrifuge-base-productivity"].value then
-    centrifuge.effect_receiver = centrifuge.effect_receiver or {}
-    centrifuge.effect_receiver.base_effect = centrifuge.effect_receiver.base_effect or {}
-    centrifuge.effect_receiver.base_effect.productivity = 0.5
-end
 
 -- Labs
 table.insert(data.raw["lab"]["lab"].inputs, "nuclear-science-pack")
